9f386843d59ed3b14bd5f92a03d09c68f14cc5da
Thossapon Anuntagool <king.thossapon@gmail.com>
2019-08-17 10:10:08 +0700

    add ID

--- app.js
+++ app.js
@@ -1,26 +1,39 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
+const express = require("express");
+const app = express();
+const port = 3000;
 app.use(express.json())
-app.get('/', (req, res) => res.send('Hello World!'))
+class Pokemons
+{
+        constructor(name,type)
+        {
+        this.id = null
+        this.name = name
+        this.type = type
+        }
 
-var pokemons=[
-    {
-        name: "KKK",
-        type: "Human"
-    },
-    {
-        name: "DDD",
-        type: "Doctor"
-    },{
-    name: "SSS",
-        type: "Support"
-    }
-]
-app.get('/pokemons', (req, res) => res.send(pokemons))
-app.post('/pokemons',(req,res)=>{
-    pokemons.push(req.body)
+}
+function generateId()
+{
+    let NewId = pokemons.length+1
+    return NewId
+}
+function createPokemon(name,type)
+{
+    let p = new Pokemons(name,type)
+    p.id = generateId()
+    return p
+}
+let pokemons = []
+    pokemons.push(createPokemon('Praduk','Fish')),
+    pokemons.push(createPokemon('Prawit','Clock'))
+
+app.get("/pokemons", (req, res) => res.send(pokemons))
+app.post("/pokemons", (req, res) => {
+    let p = new Pokemons(req.body.name , req.body.type)
+    p.id = generateId()
+    console.log(req.body)
+    pokemons.push(p)
     res.sendStatus(201)
 })
-app.listen(port, () => console.log(`Pokemon API listening on port ${port}`))
+
+app.listen(port, () => console.log(`Pokemon!! API listening on port ${port}!`))
